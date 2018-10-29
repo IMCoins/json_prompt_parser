9684aca50d488afb671825e6ace94fe32b4be3a5
IMCoins <beu.kilian@gmail.com>
2018-10-29 23:01:39 +0100

    PrettyMap seems to be finished. Lots of feature have been thought about for the foreseeable future. The aim of the main function (called decrypt) is to nicely display any Python data structure so that one can study it easily

--- PrettyMap.py
+++ PrettyMap.py
@@ -1,51 +1,83 @@
-def format_text_from_dict(value):
-	if isinstance(value, list):
-		return ''
+import json
+
+def format_text_from_iter(value):
+	if isinstance(value, list) or isinstance(value, dict) or isinstance(value, set):
+		return type(value)
 	return value
 
-def format_text_from_arr(value):
-	pass
+def format_basic_value(value):
+	if isinstance(value, str):
+		return "'{}'".format(value)
+	elif isinstance(value, int) or isinstance(value, float):
+		return "{} ({})".format(value, type(value))
 
-def decrypt_dict(data, depth):
-	message = ""
-	for key, value in data.items():
-		# message += '{}{} : {}\n'.format(depth * '\t', key, format_text_from_dict(value))
-		message += decrypt(value, depth + 1)
-	#	Going back into the parser
-	return message
-
-def decrypt_array(data, depth):
+def decrypt(data, depth=0, max_iter=None):
 	message = ''
-	for item in data:
-		message += decrypt(item, depth + 1)
-	#	Going back into the parser 
-	return message
+	if isinstance(data, list) or isinstance(data, set):
+		if depth == 0:
+			#	We only increment the depth when meeting an iterable to display.
+			#	But for the sake of user experience, we want to increment
+			#	before displaying if we meet a list as first type.
+			message += '{}{}:\n'.format(depth * '\t', type(data))
+			depth += 1
 
-def decrypt(data, depth=0):
-	if isinstance(data, list):
-		message = '{}{} : {}\n'.format(depth * '\t', type(data), '')
-		message += decrypt_array(data, depth)
-		# Going into array_function with depth += 1
+		for count, item in enumerate(data):
+			#	NOTE TO SELF : It could be a good idea to implement
+			#	a feature that would give info on list components.
+			if max_iter and max_iter <= count:
+				break
+
+			if isinstance(item, list) or isinstance(item, set):
+				#	The decrypt function in the else statement is not enough to take
+				#	care about the nested lists to display.
+				message += '{}{}:\n'.format(depth * '\t', format_text_from_iter(item))
+				message += decrypt(item, depth + 1, max_iter)
+			else:
+				message += '{}{}---\n'.format(decrypt(item, depth, max_iter), depth * '\t')
+
 	elif isinstance(data, dict):
-		# message = '{}{} :{}\n'.format(depth * '\t', type(data))
-		message = decrypt_dict(data, depth + 1)
-		# Going into dict_function with depth += 1
-	else:
-		message = '{}{}\n'.format(depth * '\t', data)
-	return message
+		for key, item in data.items():
+			message += '{}{} : {}\n'.format(depth * '\t', key, format_text_from_iter(item))
+			if isinstance(item, list) or isinstance(item, dict) or isinstance(item, set):
+				message += decrypt(item, depth + 1, max_iter)
 
-import json
+	else:
+		message = "{}{}\n".format(depth * '\t', format_basic_value(data) )
+
+	return message
 
 def load_json(file):
 	with open(file, 'r') as f:
 		return json.loads( ''.join( f.readlines() ) )
 
 if __name__ == '__main__':
-	# print( decrypt('Hello') )
-	# print()
-	# print( decrypt({ 'values' : ['Hello', ['Hi', {'key' : 'value', 'key_2' : 'value_2'}], 3] }) )
-	# print()
+	#	Printing JSON
 	data = load_json('vpods_2.json')
+	# print( decrypt(data) )
+
+	#	Printing... Python Objects !
+	data = [
+		{
+			"First" : 1,
+			"Second" : [
+				["Lots", "of", 4, "elems"],
+				4,
+				"some_string",
+				{
+					'some_key' : {
+						'some_other_key' : "I'll stop there for now"
+					}
+				}
+			],
+			"Third" : {"even some sets"}
+		},
+		{
+			"First" : 2,
+			"Second" : 2,
+			"Third" : 4
+		}
+	]
 	print( decrypt(data) )
-	# print( '\n\n\n' )
-	# print( decrypt({'First key' : 2}))
+
+	data = load_json('vpods.json')
+	# print( decrypt(data, max_iter=1) )
